239f321959bf7111cd0fc07338607fc21146454a
Christopher David <chris@arcade.city>
2021-02-19 15:59:55 -0600

    Clean up Lander canvas component

--- src/components/canvas/Lander.js
+++ src/components/canvas/Lander.js
@@ -1,31 +1,9 @@
-import { Suspense, useEffect, useRef } from 'react'
-import { extend } from 'react-three-fiber'
-// import { useFrame, useThree } from 'react-three-fiber'
-import { Water } from 'three/examples/jsm/objects/Water'
-import { Environment, MeshDistortMaterial, Sphere } from '@react-three/drei'
-import { a, useSpring } from '@react-spring/three'
-import useStore from '@/helpers/store'
-// import Effects from './Effects'
-
-const M = a(MeshDistortMaterial)
-
-extend({ Water })
+import { Suspense } from 'react'
 
 const LanderComponent = () => {
-  const router = useStore((s) => s.router)
-  const { color } = useSpring({
-    color: router.route === '/box' ? '#272727' : 'black',
-  })
-
   return (
     <Suspense fallback={null}>
-      <water />
-      {/* <Effects /> */}
       <ambientLight intensity={0.5} />
-      {/* <Sphere args={[1, 32, 32]}>
-        <M factor={2} color={color} />
-      </Sphere> */}
-      <Environment background={false} preset={'dawn'} />
     </Suspense>
   )
 }
