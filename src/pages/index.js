29587c024e91433e583200d0335d48093b815e7c
Christopher David <chris@arcade.city>
2021-02-19 15:16:06 -0600

    Add Josefin Sans font

--- src/pages/index.js
+++ src/pages/index.js
@@ -12,6 +12,12 @@ const Page = () => {
     <>
       <Lander r3f />
       <Login />
+      <div
+        style={{ fontSize: '10em' }}
+        className='font-sans font-bold top-0 left-0 right-0 flex absolute h-screen w-screen justify-center items-center text-white'
+      >
+        Sonar
+      </div>
     </>
   )
 }
