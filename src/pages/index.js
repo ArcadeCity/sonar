604898785ed1e7afeb03529d47509f54a262e38d
Christopher David <chris@arcade.city>
2021-02-20 08:52:26 -0600

    Lander form input looks good

--- src/pages/index.js
+++ src/pages/index.js
@@ -28,17 +28,35 @@ const Page = () => {
         >
           Connecting changemakers
         </p>
-        <button
-          type='button'
-          className='text-arwes2 text-xl hover:text-white mt-10 tracking-widest font-arwes inline-flex items-center px-6 py-3 border border-transparent text-base font-semibold rounded-lg shadow-sm focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500'
-          style={{
-            backgroundColor: 'rgba(6,43,45,0.65)',
-            textShadow: '0 0 4px rgb(172 249 251 / 65%)',
-            border: '1px solid #26dafd',
-          }}
-        >
-          JOIN
-        </button>
+        <div className='mt-12 flex flex-row items-center'>
+          <div className='mr-4'>
+            <label htmlFor='email' className='sr-only'>
+              Email
+            </label>
+            <input
+              type='text'
+              name='email'
+              id='email'
+              className='font-arwes tracking-widest w-64 px-6 py-4 shadow-sm focus:ring-arwes focus:border-arwes block w-72 sm:text-sm rounded-md'
+              placeholder='your@email.address'
+              style={{
+                backgroundColor: 'rgba(6,43,45,0.65)',
+                border: '1px solid rgba(6,43,45,0.85)',
+              }}
+            />
+          </div>
+          <button
+            type='button'
+            className='text-arwes2 text-xl hover:text-white tracking-widest font-arwes inline-flex items-center px-6 py-3 border border-transparent text-base font-semibold rounded-lg shadow-sm focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500'
+            style={{
+              backgroundColor: 'rgba(6,43,45,0.65)',
+              textShadow: '0 0 4px rgb(172 249 251 / 65%)',
+              border: '1px solid #26dafd',
+            }}
+          >
+            JOIN
+          </button>
+        </div>
       </div>
     </>
   )
