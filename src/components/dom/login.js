4bbe24eec007183413b90de302336b99eeba346d
Christopher David <chris@arcade.city>
2021-02-20 09:03:41 -0600

    Add react-hook-form and connect

--- src/components/dom/login.js
+++ src/components/dom/login.js
@@ -1,17 +1,15 @@
 import { useEffect } from 'react'
 import { Magic } from 'magic-sdk'
+import { useForm } from 'react-hook-form'
 
 const m = process.browser && new Magic(process.env.NEXT_PUBLIC_MAGIC_PK)
 
 const Login = () => {
-  useEffect(() => {
-    console.log('Email:', process.env.NEXT_PUBLIC_DEMO_EMAIL)
-  }, [])
-  const login = async () => {
+  const { register, handleSubmit } = useForm()
+  const login = async ({ email }) => {
+    console.log(`Logging in with ${email}`)
     try {
-      await m.auth.loginWithMagicLink({
-        email: process.env.NEXT_PUBLIC_DEMO_EMAIL,
-      })
+      await m.auth.loginWithMagicLink({ email })
       const didToken = await m.user.getIdToken()
       console.log(`Authed with didToken ${didToken}`)
     } catch (e) {
@@ -19,13 +17,17 @@ const Login = () => {
     }
   }
   return (
-    <div className='mt-12 flex flex-row items-center'>
+    <form
+      onSubmit={handleSubmit(login)}
+      className='mt-12 flex flex-row items-center'
+    >
       <div className='mr-4'>
         <label htmlFor='email' className='sr-only'>
           Email
         </label>
         <input
-          type='text'
+          ref={register}
+          type='email'
           name='email'
           id='email'
           className='font-arwes tracking-widest w-64 px-6 py-4 shadow-sm focus:ring-arwes focus:border-arwes block w-72 sm:text-sm rounded-md'
@@ -37,8 +39,7 @@ const Login = () => {
         />
       </div>
       <button
-        onClick={login}
-        type='button'
+        type='submit'
         className='text-arwes2 text-xl hover:text-white tracking-widest font-arwes inline-flex items-center px-6 py-3 border border-transparent text-base font-semibold rounded-lg shadow-sm focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500'
         style={{
           backgroundColor: 'rgba(6,43,45,0.65)',
@@ -48,7 +49,7 @@ const Login = () => {
       >
         JOIN
       </button>
-    </div>
+    </form>
   )
 }
 
